ca13df4e1ff8182ab5c0e5150ecc06abe0b85f5b
Will <will.lambert96@gmail.com>
2020-04-10 22:11:33 -0400

    add width to display

--- src/DisplayView.jsx
+++ src/DisplayView.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export default function DisplayView({ children }) {
+export default function DisplayView({ width = '100%', children }) {
   /************************************
    * Render
    ************************************/ 
 
   return (
-    <> 
+    <div style={{width: width}}> 
       {children ? children : <p>Nothing to Display.</p>}
-    </>
+    </div>
   );
 }
