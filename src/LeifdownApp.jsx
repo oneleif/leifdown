d248b5a7da85c73790cc49abe72fb3e4be079e9d
Will <will.lambert96@gmail.com>
2020-04-10 20:18:26 -0400

    Create Skeleton Navigation for leifdown views

---
+++ src/LeifdownApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import EditMode from './EditMode';
+
+export default function LeifdownApp() {
+  /************************************
+   * Render
+   ************************************/ 
+  
+  return (
+    <EditMode />
+  );
+}
